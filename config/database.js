ffb5b944ef51d4172a2dc59d087b14c5ebe232dd
sagares <santosh.sagare@cdk.com>
2017-12-04 18:48:02 +0530

    Password changed

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://sagares:S@ntosh1989@ds129936.mlab.com:29936/tasksapp',
+    remoteUrl : 'mongodb://sagares:santoshsagare@ds129936.mlab.com:29936/tasksapp',
     localUrl: 'mongodb://localhost/tasksapp',
     secret: 'h@rd2Crack'
 };
